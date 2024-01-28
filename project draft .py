753f2dd9c902f201f14b5a88644e8c93ea3f5caa
Doga <148654282+0808doga@users.noreply.github.com>
2024-01-28 15:41:51 +0100

    there is a modified and improved version of my code

--- project draft .py
+++ project draft .py
@@ -1,13 +1,44 @@
 questions = [
     {
-        "question": "2+2",
-        "answer": "4"
+        "question": "What is the formula of water in chemistry?",
+        "answer": 'H2O'
     },
     {
-        "question": "2+3",
-        "answer": "5"
-    }
+        "question": "What is the name of Newton's law?",
+        "answer": 'law of inertia'
+    },
+    {
+        "question": "Who is the painter of the Mona Lisa painting?", 
+        "answer": 'Leonardo da Vinci'
+    },
+    {
+        "question": "What is the name of the biggest ocean on the world?",
+        "answer": 'Pacific Ocean'
+    },
+    {
+        "question": 'How many planets are there in the solar system?',
+        'answer': '8 planets' 
+    },
+    {
+        'question': 'What is the name of the biggest planet in the solar system?',
+        'answer': 'Jupiter'
+    },
+    {
+        'question': 'What is the name of the biggest continent on the world?',
+        'answer': 'Asia'
+    },
+    {
+        'question': 'What is the name of the biggest country on the world?',
+        'answer': 'Russia'
+    },
 ]
+
+
+
+
+
+
+
 
 current_question_index = 0
 
@@ -16,7 +47,7 @@ while current_question_index < len(questions):
     current_answer = questions[current_question_index]["answer"]
 
     while True:
-        print("What is " + current_question + "?")
+        print(current_question + "?")
         answer = input()
 
         if answer.lower() == current_answer.lower():
